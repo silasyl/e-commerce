b0d0c6c59986468d39f9d193e239868f3b8273d7
Silas <silasyl@gmail.com>
2021-03-03 23:53:43 -0300

    e-commerce with microsservices in Java

---
+++ ecommerce-checkout-api-master/src/main/java/com/hatanaka/ecommerce/checkout/entity/CheckoutEntity.java
@@ -0,0 +1,28 @@
+package com.hatanaka.ecommerce.checkout.entity;
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Builder
+@Data
+@Entity
+public class CheckoutEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String code;
+
+    @Column
+    @Enumerated(value = EnumType.STRING)
+    private Status status;
+
+    public enum Status {
+        CREATED,
+        APPROVED
+    }
+}
